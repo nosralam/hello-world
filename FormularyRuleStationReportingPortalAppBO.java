5f07299ffc5efabb78fd8a2a7f451c071224816f
nosralam <30911448+nosralam@users.noreply.github.com>
2017-08-10 17:53:30 -0500

    Update FormularyRuleStationReportingPortalAppBO.java

--- FormularyRuleStationReportingPortalAppBO.java
+++ FormularyRuleStationReportingPortalAppBO.java
@@ -1,13 +1,1 @@
-package com.esrx.services.frsclientreport.bo;
 
-import com.esrx.services.frsclientreport.bo.Request;
-import com.esrx.services.frsclientreport.bo.Response;
-
-/**
- * File generated using ESI Mule Plug-in
- */
-public interface FormularyRuleStationReportingPortalAppBO {
-
-	Response formularyRuleStationReportingPortalMethod(Request request);
-
-}
