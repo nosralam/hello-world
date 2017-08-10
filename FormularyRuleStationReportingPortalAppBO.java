f5e7b17a506fe4c98feb56ee1126bdfefdb6c9fe
nosralam <30911448+nosralam@users.noreply.github.com>
2017-08-10 17:24:56 -0500

    Add files via upload

---
+++ FormularyRuleStationReportingPortalAppBO.java
@@ -0,0 +1,13 @@
+package com.esrx.services.frsclientreport.bo;
+
+import com.esrx.services.frsclientreport.bo.Request;
+import com.esrx.services.frsclientreport.bo.Response;
+
+/**
+ * File generated using ESI Mule Plug-in
+ */
+public interface FormularyRuleStationReportingPortalAppBO {
+
+	Response formularyRuleStationReportingPortalMethod(Request request);
+
+}
